316f97b61a261eb41019dada57112bc9e75a878b
Justin <justin@homeceuconnection.com>
2018-01-11 16:44:27 -0600

    Removed backtick notation from base of decorator

--- src/lib/Decorator/OnChange.ts
+++ src/lib/Decorator/OnChange.ts
@@ -5,14 +5,14 @@ export const OnChange: PropertyDecorator = function (target: any, key: string) {
     target['_' + key] = target[key];
     return {
         set         : function (value) {
-            let hasChanged  = this[`_${key}`] !== value;
-            this[`_${key}`] = value;
-            if (hasChanged && Value.isInstanceOf(this[`${key}Change`], EventEmitter)) {
-                this[`${key}Change`].emit(value);
+            let hasChanged  = this['_' + key] !== value;
+            this['_' + key] = value;
+            if (hasChanged && Value.isInstanceOf(this[key + 'Change'], EventEmitter)) {
+                this[key + 'Change'].emit(value);
             }
         },
         get         : function () {
-            return this[`_${key}`];
+            return this['_' + key];
         },
         enumerable  : true,
         configurable: true
